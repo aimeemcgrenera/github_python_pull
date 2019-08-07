b21a3eb801cfc1de9e180ead63ba75a72c3caa14
aimeecarney <aimee.mcgrenera@gmail.com>
2019-08-06 20:10:38 -0500

    added tests for all functions in main.py

--- test.py
+++ test.py
@@ -2,34 +2,50 @@ import unittest
 from unittest.mock import patch
 from mock import Mock
 from main import *
+import json
 
 class TestApi(unittest.TestCase):
 
     def setUp(self):
-        self.instance = GithubApiData()
-        self.__api_url = 'https://api.github.com/search/repositories'
-        self.__api_query = 'language:python'
-        self.__api_sort = 'stars'
-        self.__api_order = 'desc'
+        self.test_instance = GithubApiData()
+        self.__columns = ['repository_id', 'name', 'url', 'created_date', 'last_push_date', 'description', 'stars']
+        self.__bq_project_id = 'aerobic-ward-248616'
+        self.__dataset_name = 'outside_data'
+        self.__datatable_name = 'top_github_python_repos'
 
-    @patch('main.requests.get')
+
+    @patch('requests.get') # mock 'requests' module 'get' method
     def test_api_call(self, mock_get):
         """Mocking using a decorator"""
         mock_get.return_value.status_code = 200  # Mock status code of response.
-        response = self.instance.api_call()
+        response = self.test_instance.api_call()
         print(response.status_code)
-
         # Assert that the request-response cycle completed successfully.
         self.assertEqual(response.status_code, 200)
+        # Assert that the JSON object has data
+        self.assertIsNotNone(json.loads(response.text))
 
-    def test_process_response(self, response):
+
+    def test_process_response(self):
+        mock_response = self.test_instance.api_call()
+        mock_rows = self.test_instance.process_response(mock_response)
+        # assert type of return is list and length is 25
+        assert type(mock_rows) == list
+        assert len(mock_rows) == 25
 
 
     def test_create_df(self):
-
+        mock_response = self.test_instance.api_call()
+        mock_rows = self.test_instance.process_response(mock_response)
+        mock_df = self.test_instance.create_df(mock_rows, self.__columns)
+        # assert instance is a Pandas DataFrame
+        assert isinstance(mock_df, pd.DataFrame)
 
     def test_push_to_bq(self):
-
+        mock_df = Mock(spec=pd.DataFrame)
+        function_state = self.test_instance.push_to_bq(mock_df, self.__bq_project_id, self.__dataset_name, self.__datatable_name)
+        # assert that it hits the function_state of True
+        self.assertTrue(function_state)
 
 
 if __name__ == '__main__':
